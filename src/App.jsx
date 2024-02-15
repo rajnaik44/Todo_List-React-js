adf5e099dc3201fd022445c3986526a0a38aae6a
Your Name <raj44.naik@gmail.com>
2024-02-15 09:53:32 -1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-600">
+          hey i am a div
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
